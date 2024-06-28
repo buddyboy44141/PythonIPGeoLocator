71bb6fb46703f0d5bcf4e8c774c267d198558a99
buddyboy44141 <36573914+buddyboy44141@users.noreply.github.com>
2024-06-27 23:28:11 -0400

    Add files via upload

---
+++ IPGeoLocator.py
@@ -0,0 +1,29 @@
+import urllib, json
+import urllib.request
+
+def locate(ip):
+    with urllib.request.urlopen(f"https://ipinfo.io/{ip}/json") as url:
+        data = json.load(url)
+        print(data)
+        if "ip" in data:
+            print("IP Address:",data["ip"])    
+        if "city" in data:
+            print("City:",data["city"])
+        if "region" in data:
+            print("Region:",data["region"])
+        if "country" in data:
+            print("Country:",data["country"])
+        if "postal" in data:
+            print("Zip Code:",data["postal"])
+        if "timezone" in data:
+            print("Timezone:",data["timezone"])
+        if "hostname" in data:
+            print("Hostname:",data["hostname"])
+        if "loc" in data:
+            print("Coorinates:",data["loc"])
+        if "org" in data:
+            print("Org:",data["org"])
+
+while(True):
+    iploc = str(input("Please enter the IP Address to Geolocate: "))
+    locate(iploc)
